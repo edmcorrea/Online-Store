ba534b041e372fa157f2101f6f3f7bb4ebc06152
leandro-miranda <leandromiranda2010@gmail.com>
2022-07-07 20:54:03 -0300

    requisito 5 feito

---
+++ src/components/Card.jsx
@@ -0,0 +1,11 @@
+// import React, { Component } from 'react';
+
+// export default class Card extends Component {
+//   render() {
+//     const { list } = this.props;
+//     console.log(list);
+//     return (
+//       <div />
+//     );
+//   }
+// }
