d82b2d8242839b036559a52b5e06720ad01663dd
edmcorrea <edm.correa@hotmail.com>
2022-07-07 15:39:59 -0300

    adicionando primeiro commit modificado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 export async function getCategories() {
   // Implemente aqui
 }
-'primeiro commit aaa'
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
+
+console.log('oi');
