714edc05a1592b0dec9a5e16e6ce314d1e23bbe3
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2022-07-07 19:02:49 -0300

    requisito 4 - lint corrigido

--- src/components/CategoriesList.jsx
+++ src/components/CategoriesList.jsx
@@ -1,42 +1,41 @@
-import React from "react";
-import { getCategories } from "../services/api";
+import React from 'react';
+import { getCategories } from '../services/api';
 
 class CategoriesList extends React.Component {
-  constructor ()
-  {
+  constructor() {
     super();
     this.state = {
       categories: [],
-    }
+    };
   }
+
   componentDidMount() {
     this.categoriesAll();
   }
+
   categoriesAll = async () => {
     const categories = await getCategories();
-    this.setState({ 
-      categories,  
-    })
+    this.setState({ categories });
   }
-  render () {
-    const { categories } = this.state;    
+
+  render() {
+    const { categories } = this.state;
     return (
       <div>
         <p>Categorias</p>
         <ul>
-          {categories.map((categoria)=>(
+          { categories.map((categoria) => (
             <button
-            type="button"
-            data-testid="category" 
-            key={categoria.id}
+              type="button"
+              data-testid="category"
+              key={ categoria.id }
             >
-              {categoria.name}
+              { categoria.name }
             </button>
           ))}
         </ul>
-
       </div>
-    )
+    );
   }
 }
 
