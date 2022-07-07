655fd69911faf525bb55fd869de636df7a149ffd
edmcorrea <edm.correa@hotmail.com>
2022-07-07 15:31:11 -0300

    iniciando novo projeto

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 export async function getCategories() {
   // Implemente aqui
 }
-
+'primeiro commit'
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
 }
