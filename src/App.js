3afc58d78344f49cc24e6b8712d3320a898ccef1
edmcorrea <edm.correa@hotmail.com>
2022-07-08 16:59:15 -0300

    requisito 6 finalizado novamente

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import { getCategories, getProductsFromCategoryAndQuery } from './services/api';
 import Home from './components/Home';
 import Cart from './components/Cart';
 import Cartbutton from './components/Cartbutton';
 
 class App extends React.Component {
   render() {
-    getProductsFromCategoryAndQuery();
-    getCategories();
     return (
       <section>
         <BrowserRouter>
